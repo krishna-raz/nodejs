dd0641ce4fe978cfac4328f00d644823dd502b1f
krishna <krishnaraj143007@gmail.com>
2023-05-07 12:25:39 +0530

    full restfull api

--- resfullapi/src/db/conf.js
+++ resfullapi/src/db/conf.js
@@ -1,3 +1,3 @@
 const mogoees = require("mongoose");
 
-mogoees.connect("mongodb+srv://krishna:admin@cluster0.5exf1tj.mongodb.net/student").then(() => console.log('Connected!'));
+mogoees.connect("").then(() => console.log('Connected!'));
